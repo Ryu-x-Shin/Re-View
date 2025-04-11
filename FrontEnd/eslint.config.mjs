7a1adbc93553d12df1e19ffa859c2add306d3b50
Na2te <111sym@naver.com>
2025-04-11 19:16:10 +0900

    [REVIEW-39] 📦chore: import 제한 설정 - 저수준 모듈에서 고수준 모듈을 import 하지 못하도록 설정

--- FrontEnd/eslint.config.mjs
+++ FrontEnd/eslint.config.mjs
@@ -24,6 +24,7 @@ export default [
       'react-hooks': require('eslint-plugin-react-hooks'),
       prettier: require('eslint-plugin-prettier'),
       'unused-imports': require('eslint-plugin-unused-imports'),
+      import: require('eslint-plugin-import')
     },
     rules: {
       // Prettier와 통합하여 코드 포맷팅 오류를 ESLint 오류로 처리합니다.
@@ -45,10 +46,77 @@ export default [
 
       // 추가적인 기업 표준 규칙 (예: import 정렬, 코드 복잡도 제한 등)
       // "no-console": "warn", // 프로덕션 빌드 전 콘솔 제거 등
+      // React Bullet-Proof에 따라서 feature 간 교차 import 등을 방지하기 위한 규칙
+      // 
+      'import/no-restricted-paths': [
+        'error',
+        {
+            basePath: './',
+            zones: [
+                // disables cross-feature imports:
+                // eg. src/features/discussions should not import from src/features/comments, etc.
+                // Feature 폴더 내에서 교차 import를 방지하는 규칙
+                // ex : target에서 from에 해당하는 경로의 파일들을 import할 수 없지만, except는 가능
+                {
+                    target: './src/features/auth',
+                    from: './src/features',
+                    except: ['./auth'],
+                },
+                {
+                    target: './src/features/comments',
+                    from: './src/features',
+                    except: ['./comments'],
+                },
+                {
+                    target: './src/features/discussions',
+                    from: './src/features',
+                    except: ['./discussions'],
+                },
+                {
+                    target: './src/features/teams',
+                    from: './src/features',
+                    except: ['./teams'],
+                },
+                {
+                    target: './src/features/users',
+                    from: './src/features',
+                    except: ['./users'],
+                },
+                // 마찬가지로 features에서 app에 있는 내용을 import 할 수는 없음
+                {
+                  target: './src/features',
+                  from: './src/app',
+                },
+        
+                // e.g src/features and src/app can import from these shared modules but not the other way around
+                // component 등 공용으로 사용되어져야 하는 곳에서 features, app에 있는 것을 import 할 수 없음
+                {
+                    target: [
+                        './src/components',
+                        './src/hooks',
+                        './src/lib',
+                        './src/types',
+                        './src/utils',
+                    ],
+                    from: ['./src/features', './src/app'],
+                },
+
+                // More restrictions...
+            ],
+        },
+      ],
     },
     settings: {
       // React 버전 자동 감지
       react: { version: 'detect' },
+      'import/resolver': {
+        'node': {
+          'extensions': [
+            '.ts',
+            '.tsx'
+          ]
+        }
+      }
     },
   },
 ];
