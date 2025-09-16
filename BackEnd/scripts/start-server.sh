2bfd26220a8be81d9e625d78aa20afa4adbf0817
Ryumaker <sw4968@naver.com>
2025-09-17 03:39:19 +0900

    Simple Connection Test 2

--- BackEnd/scripts/start-server.sh
+++ BackEnd/scripts/start-server.sh
@@ -7,6 +7,6 @@ echo "------- start -------"
 docker stop mytoyproject-server || true
 docker rm mytoyproject-server || true
 docker pull $ECR_URI/mytoyproject-server:latest
-docker run -d --env-file /home/ubuntu/mytp/tp.env --name mytoyproject-server -p 8080:8080 $ECR_URI/mytoyproject-server:latest
+docker run -d -m 512m --memory-swap 1g --env-file /home/ubuntu/mytp/tp.env --name mytoyproject-server -p 8080:8080 $ECR_URI/mytoyproject-server:latest
 
 echo "------- end -------"
