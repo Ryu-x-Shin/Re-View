a45e712122b24a8af3484b5b56021fa07aa9b3b1
Ryumaker <sw4968@naver.com>
2025-09-17 00:18:19 +0900

    [BackEnd-SignUp] Fix Security Config Error

--- BackEnd/src/main/java/com/example/BackEnd/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/example/BackEnd/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
